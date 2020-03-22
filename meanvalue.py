ff8cd7e6b542d88816b72454fce19f854cb9c16d
xiaobei <1769908188@qq.com>
2020-03-22 10:17:47 +0800

    求N个数字的平均值（可以输入小数）

--- meanvalue.py
+++ meanvalue.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 count = int(input("请输入你要输入的个数："))
 i = 0
 sum = 0  # 定义变量记录和
 while i < count:
-    num = int(input("请输入数字："))
+    num = float(input("请输入数字："))
     sum += num  # 每输入一个数字都进行一次加法
     i += 1
 mean = sum / count
-print(mean)
+print("这%d个数的平均值为%0.2f" % (count, mean))
 
